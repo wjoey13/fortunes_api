dbe4f530bf86aa0eccaa5a5ca605b9eefbcccb9b
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2019-05-27 20:59:55 -0600

    finished full C.R.U.D fortune api

--- app.js
+++ app.js
@@ -21,23 +21,51 @@ app.get('/fortunes/:id', (req, res) => {
     res.json(fortunes.find(f => f.id == req.params.id));
 });
 
-app.post('/fortunes', (req, res) => {
+const writeFortunes = json => {
+    fs.writeFile('./data/fortunes.json', JSON.stringify(json), err => console.log(err));
 
+};
+
+app.post('/fortunes', (req, res) => {
     const { message, lucky_number, spirit_animal } = req.body;
 
     const fortune_ids = fortunes.map(f => f.id);
 
-    const fortune = {
+    const new_fortunes = fortunes.concat({
         id: (fortune_ids.length > 0 ? Math.max(...fortune_ids) : 0) + 1,
         message,
         lucky_number,
         spirit_animal
-    };
+    });
 
-    const new_fortunes = fortunes.concat(fortune);
+    writeFortunes(new_fortunes);
 
-    fs.writeFile('./data/fortunes.json', JSON.stringify(new_fortunes), err => console.log(err));
+    res.json(new_fortunes);
+});
 
+app.put('/fortunes/:id', (req, res) => {
+
+    const { id } = req.params;
+    const { message, lucky_number, spirit_animal } = req.body;
+
+    const old_fortune = fortunes.find(f => f.id == id);
+
+    ['message', 'lucky_number', 'spirit_animal'].forEach(key => {
+        if (req.body[key]) old_fortune[key] = req.body[key];
+    });
+
+    writeFortunes(fortunes);
+
+    res.json(fortunes);
+});
+
+app.delete('/fortunes/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    const new_fortunes = fortunes.filter(f => f.id != id);
+
+    writeFortunes(new_fortunes);
     res.json(new_fortunes);
 });
 
